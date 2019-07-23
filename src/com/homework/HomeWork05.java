b80fd4d54cb4599c0409a1383f72c833708675bf
Administrator <Administrator@EDZ-20170830ZER>
2019-07-23 11:56:29 +0800

    new content

--- src/com/homework/HomeWork05.java
+++ src/com/homework/HomeWork05.java
@@ -8,7 +8,32 @@ public class HomeWork05 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("请输入年份：");
+		int year=sc.nextInt();
 		System.out.println("请输入月份：");
+		int month=sc.nextInt();
 		System.out.println("请输入日期：");
+		int day=sc.nextInt();
+		sc.close();
+		int days=0;
+	switch(month){
+		case 12:days+=30;
+		case 11:days+=31;
+		case 10:days+=30;
+		case 9:days+=31;
+		case 8:days+=31;
+		case 7:days+=31;
+		case 6:days+=31;
+		case 5:days+=30;
+		case 4:days+=31;		
+		case 3:
+			if ((year%400==0)||(year%4==0&&year%4!=0)) {
+				days+=29;
+			}else {
+				days+=28;
+			}
+		case 2:days+=31;
+		case 1:days+=day;
 	}
+	System.out.printf("%d年%d月%d日是今年的第%d天",year,month,day,days);
+}
 }
---
+++ src/com/homework/HomeWork06.java
@@ -0,0 +1,32 @@
+package com.homework;
+import java.util.Scanner;
+/**
+ * @author Administrator
+ * 从键盘输入一个 0~99999 之间的任意数，判断输入的数是几位数？
+ */
+public class HomeWork06 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("请输入一个五位数以内的正整数");
+		int num=sc.nextInt();//假如用户输入了小数，还不会使用异常。
+		if (num/10000>=1) {
+			System.out.printf("%d是一个五位数",num);
+		}
+		else if (num/1000>=1) {
+			System.out.printf("%d是一个四位数",num);
+		}
+		else if (num/100>=1) {
+			System.out.printf("%d是一个三位数",num);
+		}
+		else if (num/10>=1) {
+			System.out.printf("%d是一个两位数",num);
+		}
+		else if (num/1>=1||num==0) {
+			System.out.printf("%d是一个一位数",num);
+		}
+		else {
+			System.out.printf("请输入正整数。");
+		}
+		sc.close();
+	}
+}
